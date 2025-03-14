547f7f6a139da09511089cf71425475388f3f88c
Paolo Salvatore Galfano <paologalfano99@gmail.com>
2025-03-14 18:38:43 +0100

    fix flow 7 run script

--- scripts/run_for_SoA_comparison.sh
+++ scripts/run_for_SoA_comparison.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-n_couples=256
+n_couples=246
 tx=30
 ty=30
 ang=25
 reps=50
 
-./generate_dataset.sh 512 256
+./generate_dataset.sh 512 246
 
 echo "> Running"
 ./host_overlay.exe $n_couples $tx $ty $ang $reps 
